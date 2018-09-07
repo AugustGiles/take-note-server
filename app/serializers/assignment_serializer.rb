fed3b5848dbb6968689784258d08a835d1be02f5
github username <github email address>
2018-09-07 09:04:32 -0400

    serialized the attached recordings to render on assignment

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,5 +1,12 @@
 class AssignmentSerializer < ActiveModel::Serializer
-  attributes :id, :teacher_id, :student_id, :assignment_text, :practice_goal, :current_practice_time, :created_at
+  attributes :id, :teacher_id, :student_id, :assignment_text, :practice_goal, :current_practice_time, :created_at, :recordings
   belongs_to :teacher, class_name: "User", foreign_key: "teacher_id"
   belongs_to :student, class_name: "User", foreign_key: "student_id"
+
+  def recordings
+    object.recordings.map { |recording|
+      Rails.application.routes.url_helpers.rails_blob_url(recording, only_path: true)
+    }
+  end
+
 end
