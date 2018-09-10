b47cb6d1e127be4a0d4159f73bbac7c1f02cca90
github username <github email address>
2018-09-10 10:38:44 -0400

    serialized the url to the attached file on resource

--- app/serializers/resources_serializer.rb
+++ app/serializers/resources_serializer.rb
@@ -3,7 +3,7 @@ class ResourcesSerializer < ActiveModel::Serializer
   belongs_to :assignment
 
   def file
-    object.file
+    Rails.application.routes.url_helpers.rails_blob_path(object.file, only_path: true)
   end
 
 end
